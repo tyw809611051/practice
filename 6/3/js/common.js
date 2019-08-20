de595b616c97a5a71b71ce6fcc40ee7c933cf800
tyw809611051 <809611051@qq.com>
2019-08-20 10:25:42 +0800

    tab

--- 6/3/js/common.js
+++ 6/3/js/common.js
@@ -39,8 +39,101 @@ var searchTop = function () {
 };
 
 var banner = function () {
+/*自动轮播且无缝*/
+    /*点药随着图片的轮播改变*/
+    /*滑动效果*/
+    /*滑动结束的时候， 滑动距离不超过屏幕的1/3 返回去 否则切换*/
 
+    var jdBanner = document.querySelector('.jd_banner');
+    var bannerWidth = jdBanner.offsetWidth;
+    var imgBox = jdBanner.querySelector('ul:first-child');
+    var pointBox = jdBanner.querySelector('ul:last-child');
+    var points = pointBox.querySelectorAll('li');
+
+    var index = 1;
+    var timeId = setInterval(function () {
+        index++;
+        // 过渡动画效果
+        imgBox.style.transition = 'all 0.2s';
+        imgBox.style.webkitTransition = 'all 0.2s';
+        // 位移动画效果
+        imgBox.style.transform = 'translateX('+(-index * bannerWidth)+'px)';
+        imgBox.style.webkitTransform = 'translateX('+(-index * bannerWidth)+'px)';
+
+    },1000);
+
+    // 最后一张动画做判断,是否定位到第一张
+    imgBox.addEventListener('transitionend',function () {
+        if (index >= 9) {
+            index = 1;
+            imgBox.style.transition = "none";
+            imgBox.style.transform = 'translateX('+(-index * bannerWidth)+'px)';
+        } else if (index < 1){
+            index = 8;
+            imgBox.style.transition = "none";
+            imgBox.style.transform = 'translateX('+(-index * bannerWidth)+'px)';
+        }
+
+        setPoint();
+    });
+
+    var setPoint = function () {
+        for (var i = 0;i < points.length; i++) {
+            var obj = points[i];
+            obj.classList.remove('now');
+
+        }
+        points[index - 1].className = "now";
+    };
+
+    // 绑定事件
+    var startX = 0;
+    var distance = 0;
+    imgBox.addEventListener('touchstart',function (e) {
+        clearInterval(timeId);
+        startX = e.touches[0].clientX;
+    });
+
+    imgBox.addEventListener('touchmove',function (e) {
+        // 移动的距离
+        var moveX = e.touches[0].clientX;
+        distance = moveX - startX;
+        var translateX = -index * bannerWidth + distance;
+
+        removeTransition();
+        setTranslateX(translateX);
+    });
+
+    imgBox.addEventListener('touchend',function (e) {
+        if (Math.abs(distance) < bannerWidth/3) {
+            addTransition();
+            setTranslateX(-index * bannerWidth);
+        } else {
+            addTransition();
+            if (distance > 0) {
+                index--;
+            } else {
+                index++;
+            }
+            setTranslateX(-index * bannerWidth);
+        }
+    });
+
+    var addTransition = function () {
+        imgBox.style.transition = 'all 0.2s';
+        imgBox.style.webkitTransition = 'all 0.2s';
+    }
+    var removeTransition = function () {
+        imgBox.style.transition = 'none';
+        imgBox.style.webkitTransition = 'none';
+    }
+    var setTranslateX = function (translateX) {
+        imgBox.style.transform = 'translateX(' + translateX + 'px)';
+        imgBox.style.webkitTransform = 'translateX(' + translateX + 'px)';
+    }
 };
+
+
 
 var downTime = function () {
 
